fdcc9674ac2d19913e07d1bf717225d18a13441c
Minh Manh Vo <vominhmanh29@gmail.com>
2021-08-17 14:00:22 +0700

    fix convention

--- js/messenger.js
+++ js/messenger.js
@@ -6,17 +6,17 @@ chatbox.setAttribute( "attribution", "biz_inbox" );
 
 window.fbAsyncInit = function ()
 {
-    FB.init( {
-        xfbml: true,
-        version: 'v11.0'
-    } );
+  FB.init( {
+    xfbml: true,
+    version: 'v11.0'
+  } );
 };
 
 ( function ( d, s, id )
 {
-    var js, fjs = d.getElementsByTagName( s )[ 0 ];
-    if ( d.getElementById( id ) ) return;
-    js = d.createElement( s ); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore( js, fjs );
+  var js, fjs = d.getElementsByTagName( s )[ 0 ];
+  if ( d.getElementById( id ) ) return;
+  js = d.createElement( s ); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore( js, fjs );
 }( document, 'script', 'facebook-jssdk' ) );
